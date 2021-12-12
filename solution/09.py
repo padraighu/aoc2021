b154efe8dc875169ec006590022e79a66b1a1015
Yifei Hu <huyifei23@gmail.com>
2021-12-11 22:25:14 -0600

    run pre commit

--- solution/09.py
+++ solution/09.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Set
+from typing import List, Set, Tuple
 
 import pytest
 from util import read_input
@@ -17,13 +17,13 @@ def find_low_points(caves: List[List[int]]) -> List[Tuple[int, int]]:
             cave = caves[i][j]
             low_point = True
             if i > 0:
-                low_point &= cave < caves[i-1][j]
+                low_point &= cave < caves[i - 1][j]
             if i < len(caves) - 1:
-                low_point &= cave < caves[i+1][j]
+                low_point &= cave < caves[i + 1][j]
             if j > 0:
-                low_point &= cave < caves[i][j-1]
+                low_point &= cave < caves[i][j - 1]
             if j < len(caves[i]) - 1:
-                low_point &= cave < caves[i][j+1]
+                low_point &= cave < caves[i][j + 1]
             if low_point:
                 res.append((i, j))
     return res
@@ -36,7 +36,10 @@ def part_one(caves: List[List[int]]) -> int:
         res += caves[i][j] + 1
     return res
 
-def find_basin(caves: List[List[int]], low_point: Tuple[int, int]) -> Set[Tuple[int, int]]:
+
+def find_basin(
+    caves: List[List[int]], low_point: Tuple[int, int]
+) -> Set[Tuple[int, int]]:
     res = set()
     queue = [low_point]
     while len(queue) > 0:
@@ -44,15 +47,16 @@ def find_basin(caves: List[List[int]], low_point: Tuple[int, int]) -> Set[Tuple[
         if caves[i][j] == 9:
             continue
         res.add((i, j))
-        if i > 0 and (i-1, j) not in res:
-            queue.append((i-1, j))
-        if i < len(caves) - 1 and (i+1, j) not in res:
-            queue.append((i+1, j))
-        if j > 0  and (i,j-1) not in res:
-            queue.append((i,j-1))
-        if j < len(caves[i]) - 1 and (i,j+1) not in res:
-            queue.append((i,j+1))
+        if i > 0 and (i - 1, j) not in res:
+            queue.append((i - 1, j))
+        if i < len(caves) - 1 and (i + 1, j) not in res:
+            queue.append((i + 1, j))
+        if j > 0 and (i, j - 1) not in res:
+            queue.append((i, j - 1))
+        if j < len(caves[i]) - 1 and (i, j + 1) not in res:
+            queue.append((i, j + 1))
     return res
+
 
 def part_two(caves: List[List[int]]) -> int:
     res = 1
@@ -65,6 +69,7 @@ def part_two(caves: List[List[int]]) -> int:
     for s in basin_sizes[:3]:
         res *= s
     return res
+
 
 @pytest.mark.parametrize("input, res", [(TEST_INPUT, 15), (REAL_INPUT, 500)])
 def test_part_one(input: str, res: int) -> None:
