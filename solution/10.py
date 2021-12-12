78ba1b82f7ca1a49a580b0ad0d39e0b9ac497ba0
Yifei Hu <huyifei23@gmail.com>
2021-12-12 00:27:09 -0600

    day 10 precommit

--- solution/10.py
+++ solution/10.py
@@ -1,10 +1,11 @@
-from typing import List, Tuple
 from collections import deque
+from typing import Deque, List
 
 import pytest
 from util import read_input
 
 TEST_INPUT, REAL_INPUT = read_input("10")
+
 
 def parse_input(input: str) -> List[str]:
     return [l for l in input.splitlines() if len(l) > 0]
@@ -12,10 +13,10 @@ def parse_input(input: str) -> List[str]:
 
 def part_one(lines: List[str]) -> int:
     res = 0
-    match = {'(': ')', '[': ']', '{': '}', '<': '>'}
-    score = {')': 3, ']': 57, '}': 1197, '>': 25137}
+    match = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    score = {")": 3, "]": 57, "}": 1197, ">": 25137}
     for l in lines:
-        stack = deque()
+        stack = deque()  # type: Deque[str]
         illegal = None
         for c in l:
             if c in match.keys():
@@ -32,12 +33,12 @@ def part_one(lines: List[str]) -> int:
 
 def part_two(lines: List[str]) -> int:
     res = 0
-    match = {'(': ')', '[': ']', '{': '}', '<': '>'}
-    score = {')': 1, ']': 2, '}': 3, '>': 4}
+    match = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    score = {")": 1, "]": 2, "}": 3, ">": 4}
     line_scores = []
     for l in lines:
         line_score = 0
-        stack = deque()
+        stack = deque()  # type: Deque[str]
         illegal = None
         for c in l:
             if c in match.keys():
@@ -48,7 +49,7 @@ def part_two(lines: List[str]) -> int:
                     illegal = c
                     break
         if not illegal:
-            autocomplete = ''.join([match[c] for c in reversed(stack)])
+            autocomplete = "".join([match[c] for c in reversed(stack)])
             for c in autocomplete:
                 line_score = 5 * line_score + score[c]
             line_scores.append(line_score)
@@ -60,6 +61,7 @@ def part_two(lines: List[str]) -> int:
 @pytest.mark.parametrize("input, res", [(TEST_INPUT, 26397), (REAL_INPUT, 362271)])
 def test_part_one(input: str, res: int) -> None:
     assert part_one(parse_input(input)) == res
+
 
 @pytest.mark.parametrize("input, res", [(TEST_INPUT, 288957), (REAL_INPUT, 1698395182)])
 def test_part_two(input: str, res: int) -> None:
